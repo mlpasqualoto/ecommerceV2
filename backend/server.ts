6d4a62a32424f1edc20020dd78b5e7787a3766e4
Matheus Leite Pasqualoto <matheus_pasqualoto@outlook.com>
2025-10-24 17:19:18 -0300

    add helmet no server - backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import helmet from "helmet";
 import connectDB from "./src/config/db";
 import orderRoutes from "./src/routes/orderRoutes";
 import productRoutes from "./src/routes/productRoutes";
@@ -10,6 +11,39 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+
+// ativar em produção
+/*
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        "default-src": ["'self'"], // somente o mesmo domínio
+        "img-src": ["'self'", "data:", "https:"], // imagens locais e externas seguras
+        "script-src": ["'self'", "https://www.googletagmanager.com", "https://www.google-analytics.com"], // scripts confiáveis
+        "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"], // CSS local e do Google Fonts
+        "font-src": ["'self'", "https://fonts.gstatic.com"], // fontes seguras
+        "connect-src": ["'self'", "https://api.stripe.com", "https://api.mercadopago.com"], // conexões para APIs externas
+      },
+    },
+    crossOriginEmbedderPolicy: false, // evita bloqueio de conteúdo de terceiros
+    crossOriginResourcePolicy: { policy: "cross-origin" }, // permite recursos externos (CDNs, imagens, etc.)
+    crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" }, // compatível com janelas popup (checkout)
+    frameguard: { action: "deny" }, // impede clickjacking
+    referrerPolicy: { policy: "no-referrer" }, // não envia referenciadores
+    hidePoweredBy: true, // remove o cabeçalho X-Powered-By
+    hsts: { maxAge: 63072000, includeSubDomains: true, preload: true }, // força HTTPS
+    noSniff: true, // impede que o navegador tente adivinhar tipo MIME
+    xssFilter: true, // ativa proteção contra XSS
+  })
+)
+*/
+
+app.use(helmet({
+  contentSecurityPolicy: false,
+  crossOriginEmbedderPolicy: false,
+}))
 
 const allowedOrigins = [
   "http://localhost:3000",
