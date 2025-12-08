a255a64cce7d19b84edbc8ddbb4f971794b142d4
Matheus Leite Pasqualoto <matheus_pasqualoto@outlook.com>
2025-12-08 09:01:25 -0300

    ajustes layout frontend - frontend-admin

--- frontend-admin/app/admin/page.jsx
+++ frontend-admin/app/admin/page.jsx
@@ -1443,7 +1443,7 @@ export default function AdminHome() {
             </form>
 
             {/* Vendedor */}
-            <div>
+            <div className="flex items-center gap-2">
               <label className="text-sm font-semibold text-slate-700 whitespace-nowrap">
                 Vendedor:
               </label>
